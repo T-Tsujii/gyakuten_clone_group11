11d65589b87844596a67b06dfd479daf9378f394
宮澤航平 <k-miyazawa@ruby-dev.jp>
2020-03-23 16:50:57 +0900

    #13 Questionモデルを作成

---
+++ db/migrate/20200323074854_create_questions.rb
@@ -0,0 +1,10 @@
+class CreateQuestions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :questions do |t|
+      t.string :title, null: false
+      t.text :detail, null:false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_003224) do
+ActiveRecord::Schema.define(version: 2020_03_23_074854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2020_03_23_003224) do
   create_table "aws_texts", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "detail", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
