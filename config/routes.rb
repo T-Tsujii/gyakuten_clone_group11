de2abab1fc2b4d75cb367bf4b9c2fb48ae1d01b3
宮澤航平 <k-miyazawa@ruby-dev.jp>
2020-03-23 10:46:06 +0900

    #9 AWS教材一覧ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/aws', to: 'aws_texts#index'
+  get '/aws/:id', to: 'aws_texts#show', as: 'aws_text'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
