744741c448f9027c28ac5938a4ee3df10fb5f2d5
宮澤航平 <k-miyazawa@ruby-dev.jp>
2020-03-23 13:53:06 +0900

    #9 ルーティングをresourcesを用いて記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get '/aws', to: 'aws_texts#index'
-  get '/aws/:id', to: 'aws_texts#show', as: 'aws_text'
+  resources :aws_texts, only: %i(index show)
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
