0ce89b99d25595b02e8c671927bee83bdc44f393
hayato sato <hayatosato@hayato-MacBook-Pro.local>
2020-03-25 01:39:17 +0900

    #14 createアクション

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,16 @@
 class QuestionsController < ApplicationController
   def index
-    @questions = Question.all.order(updated_at: :desc)
     @question = Question.new
+    @questions = Question.all.order(updated_at: :desc)
+  end
+
+  def create
+    Question.create(question_params)
+  end
+
+  private
+
+  def question_params
+      params.require(:question).permit(:title, :detail)
   end
 end
